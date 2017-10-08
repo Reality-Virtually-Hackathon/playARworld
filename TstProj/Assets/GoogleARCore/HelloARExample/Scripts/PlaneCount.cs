0d66b1e1afd9ca6821e64f500159d1f2622e9077
Hayoun Oh <hayounoh@g.harvard.edu>
2017-10-07 23:47:33 -0400

    Adding some basic AR codes

---
+++ TstProj/Assets/GoogleARCore/HelloARExample/Scripts/PlaneCount.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneCount : MonoBehaviour {
+
+    public void OnGUI()
+    {
+        int count = 0;
+
+        if (GUI.Button(new Rect(90, 210, 600, 300), new GUIContent("count"))) {
+            var planes = Object.FindObjectsOfType(typeof(GoogleARCore.TrackedPlane));
+            count = planes.Length;
+        }
+        
+    }
+}
