d8baf6f0b664f9f4b59e50d42c589c727c7dcd4a
Hayoun Oh <hayounoh@g.harvard.edu>
2017-10-08 16:31:59 -0400

    collider uses time stayed in the cube

--- TstProj/Assets/testTiles/Tiles/TileScripts/WalkerCollider.cs
+++ TstProj/Assets/testTiles/Tiles/TileScripts/WalkerCollider.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WalkerCollider : MonoBehaviour {
-    
-	//private float timeHit = 0;
+
+    //private float timeHit = 0;
+    private int dt = 0; 
 
 	public void Start (){
 		gameObject.layer = TileGameManager.walkerColLayer;
@@ -22,11 +23,29 @@ public class WalkerCollider : MonoBehaviour {
     
     public void OnTriggerEnter(Collider col)
     {
-            if (col.gameObject.layer == TileGameManager.playerLayer) {
-
-			string sentence = "Hit " + transform.parent.name;
-			print (sentence);
-			TileGameManager.printString = sentence;
-	    	}
+        dt = 0; 
+        //if (col.gameObject.layer == TileGameManager.playerLayer)
+        //{
+            //string sentence = "Hit " + transform.parent.name;
+            //print(sentence);
+            //TileGameManager.printString = sentence;
+        //}
 	}
+
+    public void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.layer == TileGameManager.playerLayer)
+            dt++;
+        if (dt > 35)
+        {
+            string sentence = "Hit " + transform.parent.name;
+            print(sentence);
+            TileGameManager.printString = sentence;
+        }
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        dt = 0; 
+    }
 }
